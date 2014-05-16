29addef2278f92b51af3a63e6ae3232814a95a8c
Darth Ton <darthton@gmail.com>
2014-05-16 10:50:03 +0300

    --- yaml --- svn_rev: 2 current_ref: refs/heads/master current_commit: 4db02cbb0cac3e21378751095a8db77bc8b2923b head_branch: refs/heads/master migrated_from: v3

--- trunk/src/Console/Main.cpp
+++ trunk/src/Console/Main.cpp
@@ -1,18 +1,28 @@
 #include "../Polychaos/PEMutator.h"
 
 #include <iostream>
+#include <windows.h>
 
 using namespace mut;
 
+std::string ToANSI( const wchar_t* str )
+{
+    char buf[512];
+    WideCharToMultiByte( CP_ACP, 0, str, -1, buf, sizeof(buf), NULL, NULL );
+    return buf;
+}
+
 void usage()
 {
-    std::cout << "Usage: ZergConsole.exe <path_to_image> [output_path]\r\n"
+    std::cout << "Usage: PolychaosConsole.exe <path_to_image> [output_path]\r\n"
               << "       path_to_image - path to the target PE image file\r\n"
               << "       output_path - resulting file, optional\r\n\r\n";
 }
 
-int main( int argc, char* argv[] )
+int main(  )
 {
+    int argc = 0;
+    wchar_t** argv = CommandLineToArgvW( GetCommandLineW(), &argc );
     if (argc < 2)
     {
         usage();
@@ -24,7 +34,7 @@ int main( int argc, char* argv[] )
         PEMutator mutant( new MutationImpl() );
 
         auto path = argv[1];
-        auto out = mutant.Mutate( path, argc > 2 ? argv[2] : "" );
+        auto out = mutant.Mutate( ToANSI( path ), argc > 2 ? ToANSI( argv[2] ) : "" );
 
         std::cout << "Successfully mutated. Result saved in '" << out << "'\r\n";
     }
